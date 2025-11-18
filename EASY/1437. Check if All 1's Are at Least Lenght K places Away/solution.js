f79260ac9a3e076d07f47606acb7838028ecb10b
GLPgaspard <gaspardglp@gmail.com>
2025-11-18 16:30:24 +0100

    feat: Add submission and proof

---
+++ EASY/1437. Check if All 1's Are at Least Lenght K places Away/solution.js
@@ -0,0 +1,16 @@
+var kLengthApart = function(nums, k) {
+
+    let lastOneIndex = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            if (lastOneIndex !== -1 && i - lastOneIndex - 1 < k) {
+                return false;
+            }
+            lastOneIndex = i;
+        }
+    }
+
+    return true;
+    
+};
